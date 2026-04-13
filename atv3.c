be173b5bdc21d22b4dfb0ae21d88d592061ca095
Luis <lhfspro@gmail.com>
2026-04-13 15:05:57 -0300

    cod_acende_um_display

--- atv3.c
+++ atv3.c
@@ -1,1 +1,20 @@
-#laksdfjçakdsfj
+void setup() {
+  DDRC = 0xFF; 
+  DDRD |= (1 << PD7);
+  DDRD |= (1 << PD2);
+  
+  #A0 = A
+  #A1 = B
+  #A2 = C
+  #A3 = D
+  #A4 = E
+  #A5 = F
+  #7 = G
+
+}
+
+void loop() {
+    PORTD |= (1 << PD7);
+    PORTC |= (1 << 0xFF);
+    PORTD |= (1 << PD2);
+}
